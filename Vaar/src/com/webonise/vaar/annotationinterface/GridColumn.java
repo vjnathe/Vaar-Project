455202f81ad3f2bfcd59e12708610e40726e69de
vijayraj <vijayraj.nathe@weboniselab.com>
2013-07-23 12:15:05 +0530

    Commit for Annotation Resolver

---
+++ Vaar/src/com/webonise/vaar/annotationinterface/GridColumn.java
@@ -0,0 +1,15 @@
+package com.webonise.vaar.annotationinterface;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Vijayraj Nathe
+ *annotation interface gridcoulmn field.
+ */
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface GridColumn { 
+}
---
+++ Vaar/src/com/webonise/vaar/utility/AnnotationResolver.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.webonise.vaar.utility;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author Vijayraj Nathe class for resolvinf fields from specified class.
+ */
+
+public class AnnotationResolver {
+
+	
+	/**
+	 * @param path for class
+	 * @return Fields Array.
+	 */
+	public Field[] getFields(String path) {
+		Class<?> classobj;
+		try {
+			classobj = Class.forName(path);
+			Field fields[] = classobj.getDeclaredFields();
+			return fields;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
